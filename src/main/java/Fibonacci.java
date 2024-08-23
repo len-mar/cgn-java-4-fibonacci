053fd62a3aaa8229845de07e94d34d175f15c642
marleneanton <the.marlene.anton@gmail.com>
2024-08-23 18:14:21 +0200

    adds alternative function & dependency

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -1,10 +1,25 @@
 public class Fibonacci {
     public static long fibonacci(long n) {
+    // recursion case: cool, but inefficient
         // base case: if 0 or 1, return n
-        if (n == 0 || n == 1) {
-            return n;
-        }
-        return fibonacci(n - 1) + fibonacci(n - 2);
-    }
+                if (n == 0 || n == 1) {
+                    return n;
+                }
+                return fibonacci(n - 1) + fibonacci(n - 2);
+            }
+    // linear case: less cool, but wayy faster
+//        if (n == 0) {
+//            return 0;
+//        }
+//        long small = 0;
+//        long large = 1;
+//        for (int i = 2; i <= n; i++) {
+//            long temp = large;
+//            large = small + large;
+//            small = temp;
+//        }
+//        return large;
+//}
+//
 }
 
--- src/test/java/FibonacciTest.java
+++ src/test/java/FibonacciTest.java
@@ -1,116 +1,22 @@
-import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.params.*;
+import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class FibonacciTest {
-    @Test
-    void fibonacciShouldReturn0When0(){
-        // GIVEN
-        long n = 0;
-        long expected = 0;
-        // WHEN
-        long actual = Fibonacci.fibonacci(n);
-        // THEN
-        assertEquals(actual, expected);
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "0,0",
+                    "1,1",
+                    "2,1",
+                    "3,2",
+                    "6,8",
+                    "7,13",
+                    "37,24157817",
+                    "49,7778742049"
+            }
+    )
+    void testFibonacci(int number, long expected){
+        assertEquals(Fibonacci.fibonacci(number), expected);
     }
-
-    @Test
-    void fibonacciShouldReturn1When1(){
-        // GIVEN
-        long n = 1;
-        long expected = 1;
-        // WHEN
-        long actual = Fibonacci.fibonacci(n);
-        // THEN
-        assertEquals(actual, expected);
-    }
-    @Test
-    void fibonacciShouldReturn1When2(){
-        // GIVEN
-        long n = 2;
-        long expected = 1;
-        // WHEN
-        long actual = Fibonacci.fibonacci(n);
-        // THEN
-        assertEquals(actual, expected);
-    }
-
-    @Test
-    void fibonacciShouldReturn2When3(){
-        // GIVEN
-        long n = 3;
-        long expected = 2;
-        // WHEN
-        long actual = Fibonacci.fibonacci(n);
-        // THEN
-        assertEquals(actual, expected);
-    }
-
-    @Test
-    void fibonacciShouldReturn3When4(){
-        // GIVEN
-        long n = 4;
-        long expected = 3;
-        // WHEN
-        long actual = Fibonacci.fibonacci(n);
-        // THEN
-        assertEquals(actual, expected);
-    }
-
-    @Test
-    void fibonacciShouldReturn5When5(){
-        // GIVEN
-        long n = 5;
-        long expected = 5;
-        // WHEN
-        long actual = Fibonacci.fibonacci(n);
-        // THEN
-        assertEquals(actual, expected);
-    }
-
-    @Test
-    void fibonacciShouldReturn8When6(){
-        // GIVEN
-        long n = 6;
-        long expected = 8;
-        // WHEN
-        long actual = Fibonacci.fibonacci(n);
-        // THEN
-        assertEquals(actual, expected);
-    }
-
-    @Test
-    void fibonacciShouldReturn13When7(){
-        // GIVEN
-        long n = 7;
-        long expected = 13;
-        // WHEN
-        long actual = Fibonacci.fibonacci(n);
-        // THEN
-        assertEquals(actual, expected);
-    }
-
-    // higher numbers
-    @Test
-    void fibonacciShouldReturn24157817When37(){
-        // GIVEN
-        long n = 37;
-        long expected = 24157817;
-        // WHEN
-        long actual = Fibonacci.fibonacci(n);
-        // THEN
-        assertEquals(actual, expected);
-    }
-
-    @Test
-    void fibonacciShouldReturn7778742049When49(){
-        // GIVEN
-        long n = 49;
-        long expected = 7778742049L;
-        // WHEN
-        long actual = Fibonacci.fibonacci(n);
-        // THEN
-        assertEquals(actual, expected);
-    }
-
 }
